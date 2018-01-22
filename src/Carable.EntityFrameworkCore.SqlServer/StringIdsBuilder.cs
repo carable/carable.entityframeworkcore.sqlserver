573b3bfd9b224167029cc0950b1e196da86b66ab
Oskar Gewalli <oskar@gewalli.se>
2018-01-22 11:25:33 +0100

    First iteration of 2.0.0 version

---
+++ src/Carable.EntityFrameworkCore.SqlServer/StringIdsBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using Microsoft.SqlServer.Server;
+
+namespace Carable.EntityFrameworkCore.SqlServer
+{
+    public class StringIdsBuilder
+    {
+        private readonly TableValuedParameterBuilder _tvp;
+
+        public StringIdsBuilder()
+        {
+            _tvp = new TableValuedParameterBuilder("dbo.StringIds",
+                    new SqlMetaData("Id", SqlDbType.NVarChar, -1));
+        }
+
+
+        public StringIdsBuilder AddRows(IEnumerable<string> rows)
+        {
+            _tvp.AddRows(rows.Select(v => new object[] { v }).ToArray());
+            return this;
+        }
+        public SqlParameter CreateParameter(string name) => _tvp.CreateParameter(name);
+        public static string TypeDeclarationSql = "CREATE TYPE dbo.StringIds AS TABLE (Id NVarChar(MAX));";
+    }
+}
---
+++ test/Tests/StringIdsBuilderTests.cs
@@ -0,0 +1,17 @@
+using Carable.EntityFrameworkCore.SqlServer;
+using Xunit;
+
+namespace Tests
+{
+    public class StringIdsBuilderTests
+    {
+        [Fact]
+        public void Smoke_test()
+        {
+            var tvp = new StringIdsBuilder()
+                .AddRows(new[] { "alpha", "beta" })
+                .CreateParameter("someids");
+            Assert.Equal("someids", tvp.ParameterName);
+        }
+    }
+}
